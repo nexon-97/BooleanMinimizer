9c856a6eb08bf89ddd7a12aa85036ede60f580da
Denis Ponyakov <nexons.97@gmail.com>
2017-12-24 02:32:04 +0300

    Mofified readme and removed redundant code from main

--- main.py
+++ main.py
@@ -3,16 +3,6 @@ from expression_generator import TableOfTruthExpressionGenerator
 from optimizer import BooleanExpressionOptimizer
 from printer import StandardExpressionPrinter
 	
-	
-def format_expression(components):
-	formatted_components = []
-	for item in components:
-		a = list(map(lambda x: x[0] if x[1] else '!' + x[0], item))
-		formatted_components.append('(%s)' % (' & '.join(a)))
-
-	return ' | '.join(formatted_components)
-	
-# =========================================================================
 
 if __name__ == "__main__":
 	from sys import argv
--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@
 
 It's a handy tool, that can load table of truth from csv file (a table sample is attached), generate all outputs boolean expressions according to the table of truth, and minimize these expressions.
 The general purpose of creation was to improve overall skill, and minimize any types of expressions quick, with purpose of creation appropriate logic schemes based on these expressions.
+
+*The attached table of truth sample implements 4-bit integers encoding into 7-segment indicator inputs.*
